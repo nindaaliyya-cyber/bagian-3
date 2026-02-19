4d076c26941a07c12ccbc2d0c2e3acac42c971b2
nindaaliyya-cyber <nindaaliyya@gmail.com>
2026-02-19 13:25:47 +0700

    Add basic calculator functionality in Java

---
+++ LatihanKalkulator.java
@@ -0,0 +1,15 @@
+public class LatihanKalkulator {
+    public static void main(String[] args) {
+        // Tipe data
+        int angka1 = 25;
+        int angka2 = 4;
+
+        // Output informasi produk
+        System.out.println("=== KALKULATOR ===");
+        System.out.println("Penjumlahan        : " + (angka1 + angka2));
+        System.out.println("Pengurangan        : " + (angka1 - angka2));
+        System.out.println("Perkalian          : " + (angka1 * angka2));
+        System.out.println("Pembagian          : " + (angka1 / angka2));
+        System.out.println("Sisa Bagi          : " + (angka1 % angka2));
+    }
+}
